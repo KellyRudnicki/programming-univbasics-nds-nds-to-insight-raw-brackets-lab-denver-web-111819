875b5f6ad23dec22d6b26a443d6b42accaa68daa
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-11 01:53:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,14 +7,14 @@ def directors_totals(nds)
  outer_array = 0
  results = {}
   
-  while outer_array < directors_database.length do 
-      directors_name = directors_database[outer_array][:name]
+  while outer_array < nds.length do 
+      directors_name = nds[outer_array][:name]
    
       directors_film_total = 0
       inner_hash = 0 
    
-      while inner_hash < directors_database[outer_array][:movies].length do
-	        directors_film_total += directors_database[outer_array][:movies][inner_hash][:worldwide_gross]
+      while inner_hash < nds[outer_array][:movies].length do
+	        directors_film_total += nds[outer_array][:movies][inner_hash][:worldwide_gross]
 		      
 		      inner_hash += 1
 		  end

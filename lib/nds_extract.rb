8ad97f08f273febc3d36d26186c8d1eb22f28972
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-11 01:50:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,12 +3,6 @@ require 'directors_database'
 
 def directors_totals(nds)
 
-
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
  directors_database
  outer_array = 0
  results = {
@@ -32,13 +26,7 @@ def directors_totals(nds)
    outer_array += 1
   end
   pp results
-  
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  # Be sure to return the result at the end!
+
   nil
 end
 
